77bf0b83bff5966eff4ddd0b659b4f6fa1230523
Fabian <acostahf4@gmail.com>
2021-09-07 12:59:09 -0500

    using canvas to draw an image and save it as a new

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("the start");
+const fs = require("fs");
+const { createCanvas, loadImage } = require("canvas");
+const canvas = createCanvas(1000, 1000);
+const ctx = canvas.getContext("2d");
+
+// function to save the image
+const saveLayer = (_canvas) => {
+  // this file takes in two things: the output path and buffer array
+  fs.writeFileSync("./newImage.png", _canvas.toBuffer("image/png"));
+  console.log("img created");
+};
+
+const drawLayer = async () => {
+  const image = await loadImage("./ADA.png");
+  //ctx.drawImage(img, x, y , width, height)
+  ctx.drawImage(image, 0, 0, 1000, 1000);
+  console.log("this ran");
+  //global canvas
+  saveLayer(canvas);
+};
+
+drawLayer();
