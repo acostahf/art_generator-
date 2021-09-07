de9ad2ece3e73b5151367eb3e431a222070861c8
Fabian <acostahf4@gmail.com>
2021-09-07 14:14:56 -0500

    creates artwork and saves it to the output folder

--- index.js
+++ index.js
@@ -2,21 +2,43 @@ const fs = require("fs");
 const { createCanvas, loadImage } = require("canvas");
 const canvas = createCanvas(1000, 1000);
 const ctx = canvas.getContext("2d");
+const { layers, width, height } = require("./input/config.js");
+// number of artworks that are going to be created
+const edition = 1;
 
 // function to save the image
-const saveLayer = (_canvas) => {
+const saveLayer = (_canvas, _edition) => {
   // this file takes in two things: the output path and buffer array
-  fs.writeFileSync("./newImage.png", _canvas.toBuffer("image/png"));
+  fs.writeFileSync(`./output/${_edition}.png`, _canvas.toBuffer("image/png"));
   console.log("img created");
 };
 
-const drawLayer = async () => {
-  const image = await loadImage("./ADA.png");
-  //ctx.drawImage(img, x, y , width, height)
-  ctx.drawImage(image, 0, 0, 1000, 1000);
-  console.log("this ran");
+const drawLayer = async (_layer, _edition) => {
+  let element =
+    //   selecting a random element for the elements array
+    _layer.elements[Math.floor(Math.random() * _layer.elements.length)];
+  // load each one the files
+  const image = await loadImage(`${_layer.location}${element.fileName}`);
+  //ctx.drawImage(img, x, y , width, height) draws image on canvas
+  ctx.drawImage(
+    image,
+    _layer.position.x,
+    _layer.position.y,
+    _layer.size.width,
+    _layer.size.height
+  );
+  console.log(
+    `I created the ${_layer.name} layer, and choose element ${element.name}`
+  );
   //global canvas
-  saveLayer(canvas);
+  saveLayer(canvas, _edition);
 };
 
-drawLayer();
+// loops the edtions
+for (let i = 1; i <= edition; i++) {
+  // for each edition draw a image
+  layers.forEach((layer) => {
+    drawLayer(layer, i);
+  });
+  console.log("creating edition" + i);
+}
